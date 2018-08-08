820fe6b53fbb1c50ea134c273a3b68d9b06bc836
刘士 <liushilive@outlook.com>
2018-08-08 11:09:20 +0800

    调整判断

--- assets/GitHubButtons.js
+++ assets/GitHubButtons.js
@@ -1,13 +1,15 @@
 require(['gitbook'], function (gitbook) {
   gitbook.events.bind('start', function (e, config) {
-    var githubURL = config.books.url || '#';
-    gitbook.toolbar.createButton({
-      icon: 'fa fa-github',
-      label: 'GitHub',
-      position: 'right',
-      onClick: function () {
-        window.open(githubURL);
-      }
-    });
+    var githubURL = config.books && config.books.url ? config.books.url : null;
+    if (githubURL) {
+      gitbook.toolbar.createButton({
+        icon: 'fa fa-github',
+        label: 'GitHub',
+        position: 'right',
+        onClick: function () {
+          window.open(githubURL);
+        }
+      });
+    }
   });
 });
